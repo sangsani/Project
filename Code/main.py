ce8bb42dfac215dda93e4e0f91be8187d7f8c7c5
biya <bi-ya@naver.com>
2023-12-13 02:22:13 +0900

    Update Code

--- Code/main.py
+++ Code/main.py
@@ -9,11 +9,11 @@ SCREEN_HEIGHT = 600
 SCREEN_WIDTH = 1100
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-RUNNING = [pygame.image.load(os.path.join("Assets/Dino", "DinoRun1.png")),
-           pygame.image.load(os.path.join("Assets/Dino", "DinoRun2.png"))]
-JUMPING = pygame.image.load(os.path.join("Assets/Dino", "DinoJump.png"))
-DUCKING = [pygame.image.load(os.path.join("Assets/Dino", "DinoDuck1.png")),
-           pygame.image.load(os.path.join("Assets/Dino", "DinoDuck2.png"))]
+RUNNING = [pygame.image.load(os.path.join("Assets/Cat", "CatRun1.png")),
+           pygame.image.load(os.path.join("Assets/Cat", "CatRun2.png"))]
+JUMPING = pygame.image.load(os.path.join("Assets/Cat", "CatJump.png"))
+DUCKING = [pygame.image.load(os.path.join("Assets/Cat", "CatDuck1.png")),
+           pygame.image.load(os.path.join("Assets/Cat", "CatDuck2.png"))]
 
 SMALL_CACTUS = [pygame.image.load(os.path.join("Assets/Cactus", "SmallCactus1.png")),
                 pygame.image.load(os.path.join("Assets/Cactus", "SmallCactus2.png")),
@@ -29,8 +29,7 @@ CLOUD = pygame.image.load(os.path.join("Assets/Other", "Cloud.png"))
 
 BG = pygame.image.load(os.path.join("Assets/Other", "Track.png"))
 
-
-class Dinosaur:
+class Cat:
     X_POS = 80
     Y_POS = 310
     Y_POS_DUCK = 340
@@ -41,66 +40,66 @@ class Dinosaur:
         self.run_img = RUNNING
         self.jump_img = JUMPING
 
-        self.dino_duck = False
-        self.dino_run = True
-        self.dino_jump = False
+        self.Cat_duck = False
+        self.Cat_run = True
+        self.Cat_jump = False
 
         self.step_index = 0
         self.jump_vel = self.JUMP_VEL
         self.image = self.run_img[0]
-        self.dino_rect = self.image.get_rect()
-        self.dino_rect.x = self.X_POS
-        self.dino_rect.y = self.Y_POS
+        self.Cat_rect = self.image.get_rect()
+        self.Cat_rect.x = self.X_POS
+        self.Cat_rect.y = self.Y_POS
 
     def update(self, userInput):
-        if self.dino_duck:
+        if self.Cat_duck:
             self.duck()
-        if self.dino_run:
+        if self.Cat_run:
             self.run()
-        if self.dino_jump:
+        if self.Cat_jump:
             self.jump()
 
         if self.step_index >= 10:
             self.step_index = 0
 
-        if userInput[pygame.K_UP] and not self.dino_jump:
-            self.dino_duck = False
-            self.dino_run = False
-            self.dino_jump = True
-        elif userInput[pygame.K_DOWN] and not self.dino_jump:
-            self.dino_duck = True
-            self.dino_run = False
-            self.dino_jump = False
-        elif not (self.dino_jump or userInput[pygame.K_DOWN]):
-            self.dino_duck = False
-            self.dino_run = True
-            self.dino_jump = False
+        if userInput[pygame.K_UP] and not self.Cat_jump:
+            self.Cat_duck = False
+            self.Cat_run = False
+            self.Cat_jump = True
+        elif userInput[pygame.K_DOWN] and not self.Cat_jump:
+            self.Cat_duck = True
+            self.Cat_run = False
+            self.Cat_jump = False
+        elif not (self.Cat_jump or userInput[pygame.K_DOWN]):
+            self.Cat_duck = False
+            self.Cat_run = True
+            self.Cat_jump = False
 
     def duck(self):
         self.image = self.duck_img[self.step_index // 5]
-        self.dino_rect = self.image.get_rect()
-        self.dino_rect.x = self.X_POS
-        self.dino_rect.y = self.Y_POS_DUCK
+        self.Cat_rect = self.image.get_rect()
+        self.Cat_rect.x = self.X_POS
+        self.Cat_rect.y = self.Y_POS_DUCK
         self.step_index += 1
 
     def run(self):
         self.image = self.run_img[self.step_index // 5]
-        self.dino_rect = self.image.get_rect()
-        self.dino_rect.x = self.X_POS
-        self.dino_rect.y = self.Y_POS
+        self.Cat_rect = self.image.get_rect()
+        self.Cat_rect.x = self.X_POS
+        self.Cat_rect.y = self.Y_POS
         self.step_index += 1
 
     def jump(self):
         self.image = self.jump_img
-        if self.dino_jump:
-            self.dino_rect.y -= self.jump_vel * 4
+        if self.Cat_jump:
+            self.Cat_rect.y -= self.jump_vel * 4
             self.jump_vel -= 0.8
         if self.jump_vel < - self.JUMP_VEL:
-            self.dino_jump = False
+            self.Cat_jump = False
             self.jump_vel = self.JUMP_VEL
 
     def draw(self, SCREEN):
-        SCREEN.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
+        SCREEN.blit(self.image, (self.Cat_rect.x, self.Cat_rect.y))
 
 
 class Cloud:
@@ -168,7 +167,7 @@ def main():
     global game_speed, x_pos_bg, y_pos_bg, points, obstacles
     run = True
     clock = pygame.time.Clock()
-    player = Dinosaur()
+    player = Catsaur()
     cloud = Cloud()
     game_speed = 20
     x_pos_bg = 0
@@ -221,7 +220,7 @@ def main():
         for obstacle in obstacles:
             obstacle.draw(SCREEN)
             obstacle.update()
-            if player.dino_rect.colliderect(obstacle.rect):
+            if player.Cat_rect.colliderect(obstacle.rect):
                 pygame.time.delay(2000)
                 death_count += 1
                 menu(death_count)
